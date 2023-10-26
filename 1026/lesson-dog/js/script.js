c438b652895b32127e548e0af655c949bc93ae9c
saske220 <146696108+saske220@users.noreply.github.com>
2023-10-26 11:26:17 +0900

    komitto
    komitto

---
+++ 1026/lesson-dog/js/script.js
@@ -0,0 +1,10 @@
+//柴犬と秋田犬の画像を取得
+const pochi = document.querySelector("#pochi");
+const hachiko = document.querySelector("#hachiko");
+
+//柴犬カードに柴犬の画像（変数）を挿入する
+const pochisCard = document.querySelector(".card1");
+pochisCard.appendChild(pochi);
+//秋田犬カードに秋田犬の画像を挿入する
+const hachikoCard = document.querySelector(".card2");
+hachikoCard.appendChild(hachiko);
